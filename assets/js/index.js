f0833e8d812721234bd81264bef0e39b37abae33
Rmd-DW <r.munizaga.d@gmail.com>
2024-04-08 19:19:37 -0400

    implementación js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,53 @@
-// Validación formulario con JavaScript
-
 document.addEventListener("DOMContentLoaded", function () {
-
     // Configuración de Bootstrap Tooltips
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-    tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl);
-    });
-
-  
-
-    // Manejo del evento submit del formulario de contacto al validar el que se llenen los campos 
-   
-    document.getElementById("fcontacto").addEventListener("submit", function(event) {
-        event.preventDefault();
-        var nombre = document.getElementById("inombre").value;
-        var asunto = document.getElementById("iasunto").value;
-        var mensaje = document.getElementById("imensaje").value;
-        if(nombre === "" || asunto === "" || mensaje === ""){
-            alert("Complete todos los campos");
-        } else {
-            alert("Mensaje enviado con éxito");
-        }
-    });
-
-
-    // Manejo del evento clic en elementos con la clase .p-toggle
-    document.querySelectorAll(".p-toggle").forEach(function (element) {
-        element.addEventListener("click", function () {
-            this.closest('.card-body').querySelector('.toggle').classList.toggle("fold", 1000);
+    var tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    if (tooltipTriggerList.length > 0) {
+        tooltipTriggerList.forEach(function(tooltipTriggerEl) {
+            new bootstrap.Tooltip(tooltipTriggerEl);
         });
-    });
+    }
+
+    // Manejo del evento cambio para el checkbox de términos y condiciones
+    var checkboxTerminos = document.getElementById('flexCheckIndeterminate');
+    if (checkboxTerminos) {
+        checkboxTerminos.addEventListener('change', function() {
+            if (this.checked) {
+                var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+                    keyboard: false
+                });
+                myModal.show();
+            }
+        });
+    }
+
+    // Validación del formulario al intentar enviarlo
+    var formularioContacto = document.getElementById("enviarCorreo");
+    if (formularioContacto) {
+        formularioContacto.addEventListener("submit", function(event) {
+            event.preventDefault();
+            var nombre = document.getElementById("inombre").value;
+            var asunto = document.getElementById("iasunto").value;
+            var mensaje = document.getElementById("imensaje").value;
+            if (nombre === "" || asunto === "" || mensaje === "") {
+                alert("Complete todos los campos");
+            } else {
+                alert("Mensaje enviado con éxito");
+            }
+        });
+    }
+
+
+
+    // Manejo del evento clic en elementos con la clase .p-toggle para mostrar/ocultar contenido
+    var elementosToggle = document.querySelectorAll(".p-toggle");
+    if (elementosToggle.length > 0) {
+        elementosToggle.forEach(function(element) {
+            element.addEventListener("click", function () {
+                var toggle = this.closest('.card-body').querySelector('.toggle');
+                if (toggle) {
+                    toggle.classList.toggle("fold");
+                }
+            });
+        });
+    }
 });
